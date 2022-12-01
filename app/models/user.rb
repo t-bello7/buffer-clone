05dd840322b80d8bfa7eec91627551a1d82d8f4d
t-bello7 <bello4aus@gmail.com>
2022-12-01 12:01:29 +0100

    add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
     has_secure_password
     #adds password:string and password_confirmation: string virtual attributes
     
+    validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'must be a valid email'}
 end
