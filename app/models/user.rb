5d867ee608d0cd5e4646933b7bb57532bcf73f89
t-bello7 <bello4aus@gmail.com>
2022-12-01 10:59:52 +0100

    - Url helps and link_to - create user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@
 class User < ApplicationRecord
     has_secure_password
     #adds password:string and password_confirmation: string virtual attributes
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   # GET about
-  get "about", to: "about#index"
+  get "about-us", to: "about#index", as: :about
   
   # main controller
   root to: "main#index"
