f301d0c36480b23c736ffe069e585103b05b21b2
t-bello7 <bello4aus@gmail.com>
2022-12-18 19:16:52 +0100

    update current user password

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/contact', as: :contact
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  # GET about
+  get 'pages/contact', as: :contact
   get "about-us", to: "about#index", as: :about
 
   get "sign_up", to: "registration#new"
@@ -13,6 +10,9 @@ Rails.application.routes.draw do
   get "sign_in", to: "sessions#new"
   post "sign_in", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
+  
+  get "password", to: "password#edit", as: :edit_password
+  patch "password", to: "password#update"
   
   # main controller
   root to: "main#index"
