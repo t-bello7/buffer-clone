458190976581c6f12ccf043c274373acb0de25ed
t-bello7 <bello4aus@gmail.com>
2022-12-18 13:41:38 +0100

    create login form

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
 
   get "sign_up", to: "registration#new"
   post "sign_up", to: "registration#create"
+
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
   
   # main controller
